2dd6602dddfbf29e88d781e357d0a2472fcae499
Wayne Dahlberg <waynefj40@gmail.com>
2024-10-07 10:09:37 -0600

    Initial Commit

---
+++ CalcPlayground/CalcPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcPlaygroundApp.swift
+//  CalcPlayground
+//
+//  Created by Wayne Dahlberg on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CalcPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
